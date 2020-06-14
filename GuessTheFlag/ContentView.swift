602a4e9da498f2ef02995d2ff8663a49e470036f
Cathal Farrell <cathalproductions@gmail.com>
2020-06-14 12:28:10 +0100

    Accessibility added with flag descriptions in labels

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -27,6 +27,22 @@ struct FlagImage: View {
 
 struct ContentView: View {
 
+    // MARK: - Accessibility
+    
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
 
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -105,6 +121,7 @@ struct ContentView: View {
                         // When you tap the correct flag, make it spin around 360 degrees on the Y axis.
                         .rotation3DEffect(.degrees(self.rotations[number]), axis: (x: 0, y: 1, z: 0))
                         .scaleEffect(self.scales[number])
+                        .accessibility(label: Text(self.labels[self.countries[number], default: "Unknown flag"]))
                     }
                     //Want to ensure that animation doesnt run - i.e backwards when starting new game
                     .animation(self.isNewGame ? nil : .default)
